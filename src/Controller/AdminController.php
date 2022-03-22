109d1428cde30bf9e87ba9d556ff7048c45fdc4c
Xyapode <okaddour@outlook.com>
2022-03-22 16:53:38 +0100

    Access control + front control + back redirection + add author relation + add table to dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,32 +2,58 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use App\Form\ArticleFormType;
 use DateTime;
+use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Entity\User;
+use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-#[Route("/admin")]
+/**
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
 
     // #[Route("/tableau-de-bord", name:"show_dashboard", methods:["GET"])]
+    
     /**
      * @Route("/tableau-de-bord", name="show_dashboard", methods={"GET"})
+     * // IsGranted("ROLE_ADMIN")
      */
     public function showDashboard(EntityManagerInterface $entityManager): Response
     {
+        /*
+        * try/catch fait partie nativement de PHP
+        * Cela a été créé pour gérer les class Exception (erreur)
+        * On se sert d'un try/catch lorsqu'on utilise des méthodes (fonctions) QUI LANCENT (throw) une Exception
+        * Si la méthode lance l'erreur pendant son exécution, alors l'Exception sera 'attrapée' (catch) Le code dans les accolades du catch sera alors exécuté.
+        */
+        try {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
+        catch(AccessDeniedException $exception){
+
+            $this->addFlash('warning', 'Cette partie du site est réservée aux administrateurs.');
+            return $this->redirectToRoute('default_home');
+        }
+
         $articles = $entityManager->getRepository(Article::class)->findAll();
+        $categories = $entityManager->getRepository(Categorie::class)->findAll();
+        $users = $entityManager->getRepository(User::class)->findAll();
 
         return $this->render('admin/show_dashboard.html.twig', [
             'articles' => $articles,
-            
+            'categories' => $categories,    
+            'users' => $users    
         ]);
     }
 
@@ -51,6 +77,10 @@ class AdminController extends AbstractController
             $article->setAlias($slugger->slug($article->getTitle() ) );
             $article->setCreatedAt(new DateTime());
             $article->setUpdatedAt(new DateTime());
+            
+            // Association d'un auteur à un article
+            // $this->getUser() retourne un objet de type UserInterface
+            $article->setAuthor($this->getUser());
 
             // Variabilisation du fichier 'photo' uploadé
             $file = $form->get('photo')->getData();
